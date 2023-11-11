46e9bac442f6b490375e89191b6a11c2fa6814c6
dhaiphan <hai.phan@duodecim.fi>
2023-11-11 10:58:17 +0200

    creating chat pge

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,11 +1,27 @@
 import "./App.css";
-import SignIn from "./component/SignIn";
-//import Home from "./component/Home";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Chat from "./components/Chat";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/chat",
+    element: <Chat/>
+  }
+]);
 
 function App() {
   return (
     <div className="App">
-      <SignIn />
+      {/* <SignIn /> */}
+      <RouterProvider router={router} />
+
     </div>
   );
 }
