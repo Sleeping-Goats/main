8f42bf644bdd8dae222e0d5440ea263479ab09a0
AnikaTahashin10 <anikatahashin90@gmail.com>
2023-11-11 14:26:33 +0200

    the info popover

--- ui/src/component/Header.jsx
+++ ui/src/component/Header.jsx
@@ -1,97 +1,75 @@
-import React, {useState} from 'react';
+import React from 'react';
+import Popover from '@mui/material/Popover';
+import Typography from '@mui/material/Typography';
 import { FaInfoCircle } from 'react-icons/fa'; // Import the info icon of your choice
 
-
-
-
-
 const Header = ({ userName }) => {
-    const [isPopoverOpen, setPopoverOpen] = useState(false);
-  
-    const handleInfoClick = () => {
-      setPopoverOpen(!isPopoverOpen);
-    };
-  
-    return (
-      <header style={styles.header}>
-        <div style={styles.centerContent}>
-          <div style={styles.userName}>{userName}</div>
-        </div>
-        <div style={styles.rightIcons}>
-          <div style={styles.iconContainer} onClick={handleInfoClick}>
-            <FaInfoCircle style={styles.icon} />
-            {isPopoverOpen && (
-              <div style={styles.popover}>
-                {"life in a dream house"}
-                Info Popover Content
-              </div>
-            )}
-          </div>
-        </div>
-      </header>
-    );
+    userName = "Sleeping Goat"
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleInfoClick = (event) => {
+    setAnchorEl(event.currentTarget);
   };
 
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
+  const open = Boolean(anchorEl);
+  const id = open ? 'info-popover' : undefined;
 
-
-
-
-
-
-// const Header = ({ userName }) => {
-//     userName = "Sleeping Goat";
-//   return (
-//     <header style={styles.header}>
-//       <div style={styles.centerContent}>
-//         <div style={styles.userName}>{userName}</div>
-//       </div>
-//       <div style={styles.rightIcons}>
-//         <FaInfoCircle style={styles.icon} />
-//       </div>
-//     </header>
-//   );
-// };
-
-const styles = {
-  header: {
-    backgroundColor: '#8DC1E6',
-    color: '#fff',
-    padding: '1rem',
-    display: 'flex',
-    justifyContent: 'space-between', // Spacing between center and right content
-    alignItems: 'center',
-    fontSize: '30px',
-  },
-  centerContent: {
-    flex: 1,
-    textAlign: 'center',
-  },
-  userName: {
-    fontWeight: 'bold',
-  },
-  rightIcons: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-  icon: {
-    fontSize: '1.5rem',
-    cursor: 'pointer',
-    position: 'relative',
-  },
-  popover: {
-    position: 'absolute',
-    top: '100%',
-    left: '50%',
-    transform: 'translateX(-50%)',
-    backgroundColor: '#fff',
-    padding: '1rem',
-    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-    borderRadius: '4px',
-    display: 'none', // Initially hide the popover
-  },
+  return (
+    <header style={styles.header}>
+      <div style={styles.centerContent}>
+        <div style={styles.userName}>{userName}</div>
+      </div>
+      <div style={styles.rightIcons}>
+        <div style={styles.iconContainer} onClick={handleInfoClick}>
+          <FaInfoCircle style={styles.icon} />
+        </div>
+        <Popover
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+        >
+          <Typography sx={{ p: 2 }}>THE SLEEPING GOAT</Typography>
+        </Popover>
+      </div>
+    </header>
+  );
 };
 
-
+const styles = {
+    header: {
+            backgroundColor: '#8DC1E6',
+            color: '#fff',
+            padding: '1rem',
+            display: 'flex',
+            justifyContent: 'space-between', // Spacing between center and right content
+            alignItems: 'center',
+            fontSize: '30px',
+          },
+          centerContent: {
+            flex: 1,
+            textAlign: 'center',
+          },
+          userName: {
+            fontWeight: 'bold',
+          },
+          rightIcons: {
+            display: 'flex',
+            alignItems: 'center',
+          },
+          icon: {
+            fontSize: '1.5rem',
+            cursor: 'pointer',
+            position: 'relative',
+          },
+};
 
 export default Header;
