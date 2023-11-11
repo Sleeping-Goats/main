a5381b36d7605928bb0c2ec874ac0cb7ee1aba8d
dhaiphan <hai.phan@duodecim.fi>
2023-11-11 11:19:18 +0200

    messages input works

--- ui/src/components/Chat.js
+++ ui/src/components/Chat.js
@@ -1,10 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Button, Typography } from "@mui/material";
 import logo from "../assets/goat.png";
-
+import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+} from "@chatscope/chat-ui-kit-react";
 
 const Chat = () => {
   const [chatting, setChatting] = useState(true);
+
+  const [messages, setMessages] = useState([]);
+  console.log("🚀 ~ file: Chat.js:17 ~ Chat ~ messages:", messages);
+
+  // Example of adding a new message (you might get these from an API or user input)
+  useEffect(() => {
+    const newMessage = {
+      message: "Hello, this is a test message!",
+      sentTime: "just now",
+      sender: "Joe",
+    };
+    setMessages([...messages, newMessage]);
+  }, []); // Empty dependency array means this runs once when the component mounts
+
+  const handleSendMessage = (newMessage) => {
+    const messageObject = {
+      message: newMessage, // the message text
+      sentTime: new Date().toLocaleTimeString(), // current time
+      sender: "User", // hardcoded sender name, you can change this based on your application's logic
+    };
+    console.log(
+      "🚀 ~ file: Chat.js:34 ~ handleSendMessage ~ messageObject:",
+      messageObject
+    );
+
+    setMessages((messages) => [...messages, messageObject]); // Update the messages array
+  };
   return chatting ? (
     <Box>
       <Box height={10} />
@@ -23,7 +57,30 @@ const Chat = () => {
     <Box>
       {/* Content for else case goes here */}
       <Box height={20} />
+      <div style={{ position: "relative", height: "500px" }}>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList>
+              {messages.map((msg, index) => (
+                <Message
+                  key={index}
+                  model={{
+                    message: msg.message,
+                    sentTime: msg.sentTime,
+                    sender: msg.sender,
+                  }}
+                />
+              ))}
+            </MessageList>
 
+            <MessageInput
+              placeholder="Type message here"
+              onSend={handleSendMessage}
+            />
+          </ChatContainer>
+        </MainContainer>
+      </div>
+      ;
       <Button onClick={() => setChatting(true)} variant="contained">
         back
       </Button>
@@ -32,4 +89,3 @@ const Chat = () => {
 };
 
 export default Chat;
-
